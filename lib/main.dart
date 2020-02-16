dd1e9d9668635f282731ec6e2bb337a0207aa5e2
Artur Movsisyan <arturmovsisyan@workfront.com>
2020-02-16 11:36:23 +0400

    small styling

--- lib/main.dart
+++ lib/main.dart
@@ -46,15 +46,28 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
             children: transactions.map((tx) {
-              return Card(child: Row(
+              return Card(
+                  child: Row(
                 children: <Widget>[
                   Container(
-                    child: Text(tx.amount.toString()),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 1)),
+                    padding: EdgeInsets.all(10),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tx.title),
-                      Text(tx.createdAt.toIso8601String()),
+                      Text(tx.title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),),
+                      Text(tx.createdAt.toIso8601String(), style: TextStyle(color: Colors.grey, fontSize: 14),),
                     ],
                   )
                 ],
