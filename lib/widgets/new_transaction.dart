6ab4010a2d347ccbced6e057e36419007a88fd0a
Artur Movsisyan <arturmovsisyan@workfront.com>
2020-02-20 21:11:27 +0400

    simple validation

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -21,18 +21,19 @@ class NewTransaction extends StatelessWidget {
               onChanged: (value) {
                 this.titleInput = value;
               },
+              onSubmitted: (_) => this.onSubmit(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
+              keyboardType: TextInputType.number,
               onChanged: (value) {
                 this.amountInput = value;
               },
+              onSubmitted: (_) => this.onSubmit(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
-              onPressed: () {
-                this.addNewTransaction(this.titleInput, double.parse(this.amountInput));
-              },
+              onPressed: this.onSubmit,
               textColor: Colors.purple,
               color: Colors.lightBlueAccent,
             )
@@ -41,5 +42,11 @@ class NewTransaction extends StatelessWidget {
         padding: EdgeInsets.all(10),
       ),
     );
+  }
+
+  onSubmit() {
+    if (this.titleInput.length > 0 && this.amountInput.length > 0 && double.parse(this.amountInput) > 0) {
+      this.addNewTransaction(this.titleInput, double.parse(this.amountInput));
+    }
   }
 }
